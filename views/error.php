463ed702962f42ce6d21e82bec6aeecefef2a96f
baptiste-mignan <baptiste.mignan@gmail.com>
2025-03-07 21:26:41 +0100

    restructuration, suppression regles inutiles et modifications

--- views/error.php
+++ views/error.php
@@ -5,8 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Erreur - IUTables'O</title>
-    <link rel="stylesheet" href="/assets/styles.css">
-    <link rel="stylesheet" href="/assets/global.css">
+    <link rel="stylesheet" href="/assets/css/global.css">
     <link rel="shortcut icon" href="/assets/favicon.ico" type="image/x-icon" />
 </head>
 
