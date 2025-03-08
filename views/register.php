4f5cb13ea7390e90f84dd7c9a9988231998d8268
baptiste-mignan <baptiste.mignan@gmail.com>
2025-03-08 13:59:59 +0100

    séparation du css et réorganisation

--- views/register.php
+++ views/register.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register - IUTables'O</title>
-    <link rel="stylesheet" href="/assets/css/styles.css">
     <link rel="stylesheet" href="/assets/css/global.css">
+    <link rel="stylesheet" href="/assets/css/login.css">
     <link rel="shortcut icon" href="/assets/favicon.ico" type="image/x-icon" />
 </head>
 
@@ -52,7 +52,7 @@
                     </tr>
                     <tr class="login-row">
                         <td class="login-label"></td>
-                        <td><input type="submit" value="Se connecter" class="button login-button login-field"></td>
+                        <td><input type="submit" value="Se connecter" class="button login-button"></td>
                     </tr>
                     <?php if (isset($err)): ?>
                         <tr>
